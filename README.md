50d0e0b88a936221a884f0db82580dcb78c0bac8
kronosGR <geo.elgeo@gmail.com>
2021-12-28 20:59:24 +0100

    Added loggin with morgan and README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+#Node.js NASA API and Front-End
+
+##More Info
+* Node.js
+* Middlewares (custom, CORS, Morgan)
+* Logging with Morgan
--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
+app.use(morgan('combined'))
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
