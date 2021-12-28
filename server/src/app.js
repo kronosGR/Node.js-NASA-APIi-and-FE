9321ce3550fefc33e31cf724bd7a6b5fe08d155f
kronosGR <geo.elgeo@gmail.com>
2021-12-28 20:51:45 +0100

    Add deploy script

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
+const path = require('path');
 
 const app = express();
 
@@ -8,8 +9,12 @@ app.use(cors({
   origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use(planetsRouter)
+app.use(planetsRouter);
+app.get('/', (req, res) => {
+  res.sendfile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 
 module.exports = app;
