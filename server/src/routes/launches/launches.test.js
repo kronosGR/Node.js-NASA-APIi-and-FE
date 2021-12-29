e0c0d97d4f456379752d19655ddcc9b7454cb204
kronosGR <geo.elgeo@gmail.com>
2021-12-29 21:25:02 +0100

    added Test for POST

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -9,8 +9,28 @@ describe('Test GET /launches', () => {
 });
 
 describe('Test POST /launch', () => {
-  test('It should respond with 200 success', () => {
+  const completeLaunchData = {
+    mission: 'USS',
+    rocket: 'Rocket',
+    target: 'Kepler-186',
+    launchDate: 'January 12, 2100'
+  };
 
+  const launchDataWithoutDate = {
+    mission: 'USS',
+    rocket: 'Rocket',
+    target: 'Kepler-186',
+  };
+
+  test('It should respond with 201 success', async () => {
+    const response = await request(app).post('/launches').send(completeLaunchData)
+      .expect('Content-Type', /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    expect(responseDate).toBe(requestDate);
+    expect(response.body).toMatchObject(launchDataWithoutDate);
   });
 
   test('It should catch missing required properties', () => {
