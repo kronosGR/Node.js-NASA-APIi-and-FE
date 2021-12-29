84a86b24c487cd59ac24ac06b3ef264847a6605e
kronosGR <geo.elgeo@gmail.com>
2021-12-29 18:08:43 +0100

    Updated FE for POST /launches

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -15,8 +15,19 @@ async function httpGetLaunches() {
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch (e) {
+    return {
+      ok: false
+    };
+  }
 }
 
 async function httpAbortLaunch(id) {
--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
   mission: 'Kepler Exploration X',
   rocker: 'Explorer',
   launchDate: new Date('December 27, 2030'),
-  destination: 'Kepler-442',
+  target: 'Kepler-442',
   customer: [ 'ZTM', 'NASA' ],
   upcoming: true,
   success: true,
