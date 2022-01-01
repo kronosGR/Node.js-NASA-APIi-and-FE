46d9451dc8b4069140e6cd7cbcbd9af813983c09
kronosGR <geo.elgeo@gmail.com>
2022-01-01 13:45:16 +0100

    get latest flight number

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,15 +1,16 @@
 const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
 
-//const launches = new Map();
+const DEFAULT_FLIGHT_NUMBER = 100;
 
-let latestFlightNumber = 100;
+const launches = new Map();
 
 const launch = {
   flightNumber: 100,
   mission: 'Kepler Exploration X',
   rocker: 'Explorer',
   launchDate: new Date('December 27, 2030'),
-  target: 'Kepler-442',
+  target: 'Kepler-442 b',
   customers: [ 'ZTM', 'NASA' ],
   upcoming: true,
   success: true,
@@ -21,6 +22,18 @@ function existsLaunchWithId(launchId) {
   return launches.has(launchId);
 }
 
+async function getLatestFlightNumber() {
+  const latestLaunch = await launchesDatabase
+    .findOne()
+    .sort('-flightNumber'); // Sorting descending with -
+
+  if (!latestLaunch) {
+    return DEFAULT_FLIGHT_NUMBER;
+  }
+
+  return latestLaunch.flightNumber;
+}
+
 async function getAllLaunches() {
   return await launchesDatabase.find({}, {
     '_id': 0, '__v': 0
@@ -28,6 +41,14 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target
+  });
+
+  if (!planet) {
+    throw new Error('No matching planet found!');
+  }
+
   await launchesDatabase.updateOne({
     flightNumber: launch.flightNumber
   }, launch, {
