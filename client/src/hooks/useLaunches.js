ac84331660ba93db145a6e672b97a4cd4b3fa70b
kronosGR <geo.elgeo@gmail.com>
2022-01-06 09:28:43 +0100

    fixed client bugs

--- client/src/hooks/useLaunches.js
+++ client/src/hooks/useLaunches.js
@@ -21,7 +21,7 @@ function useLaunches(onSuccessSound, onAbortSound, onFailureSound) {
 
   const submitLaunch = useCallback(async (e) => {
     e.preventDefault();
-    // setPendingLaunch(true);
+    setPendingLaunch(true);
     const data = new FormData(e.target);
     const launchDate = new Date(data.get("launch-day"));
     const mission = data.get("mission-name");
@@ -47,12 +47,11 @@ function useLaunches(onSuccessSound, onAbortSound, onFailureSound) {
   }, [getLaunches, onSuccessSound, onFailureSound]);
 
   const abortLaunch = useCallback(async (id) => {
-    //const response = await httpAbortLaunch(id);
+    const response = await httpAbortLaunch(id);
 
-    // TODO: Set success based on response.
-    const success = false;
+    const success = response.ok;
     if (success) {
-      await getLaunches();
+      getLaunches();
       onAbortSound();
     } else {
       onFailureSound();
